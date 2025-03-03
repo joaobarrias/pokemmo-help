e6b015e5e6dd60c8423ecdc3013e55be6216e670
joaobarrias <bjcanelas@gmail.com>
2025-03-03 22:51:47 +0000

    Force refresh for service worker.

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     },
     VitePWA({
       registerType: 'autoUpdate', // Auto-update service worker
+      filename: 'sw-v2.js',
       // Remove precache of all images
       includeAssets: [], // Only include assets explicitly imported (none here)
       manifest: {
